5184595e83555d74ae2ac1a1205fce4b5fd14a8e
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-13 17:55:01 -0600

    Definicion de la interfaz PriorityQueue

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,3 +1,8 @@
-public class PriorityQueue {
-    
+public interface PriorityQueue<E extends Comparable<E>> {
+    void add(E value);
+    E remove();
+    E getFirst();
+    boolean isEmpty();
+    int size();
+    void clear();
 }
