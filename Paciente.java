a5a13ca8f694542096a6394575bd94e79b5a6a56
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-13 17:46:41 -0600

    Implementacion de Comparable en la clase Paciente

--- Paciente.java
+++ Paciente.java
@@ -1,7 +1,13 @@
-public class Paciente {
+public class Paciente implements Comparable<Paciente> {
     private String nombre;
     private String sintoma;
     private String codigoEmergencia;
+
+    @Override
+    public int compareTo(Paciente otro) {
+        // Retorna un valor negativo si este paciente tiene mayor prioridad 
+        return this.codigoEmergencia.compareTo(otro.getCodigoEmergencia());
+    }
 
     public Paciente(String nombre, String sintoma, String codigoEmergencia) {
         this.nombre = nombre.trim();
@@ -17,4 +23,6 @@ public class Paciente {
     public String toString() {
         return nombre + ", " + sintoma + ", " + codigoEmergencia;
     }
+
+    
 }
