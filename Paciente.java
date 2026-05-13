892625371f82d5f0125c773040c4a84ef3cd28f4
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-13 17:36:33 -0600

    Clase paciente cn atributos basicos

--- Paciente.java
+++ Paciente.java
@@ -1,3 +1,20 @@
 public class Paciente {
-    
+    private String nombre;
+    private String sintoma;
+    private String codigoEmergencia;
+
+    public Paciente(String nombre, String sintoma, String codigoEmergencia) {
+        this.nombre = nombre.trim();
+        this.sintoma = sintoma.trim();
+        this.codigoEmergencia = codigoEmergencia.trim();
+    }
+
+    public String getCodigoEmergencia() {
+        return codigoEmergencia;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + codigoEmergencia;
+    }
 }
