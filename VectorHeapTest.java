8fd7e2115efde24398c54bcd39b4ad974dca835f
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-13 19:35:12 -0600

    Pruebas unitarias

--- VectorHeapTest.java
+++ VectorHeapTest.java
@@ -1,3 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class VectorHeapTest {
-    
+
+    @Test
+    public void testAddAndRemove() {
+        VectorHeap<Paciente> heap = new VectorHeap<>();
+        heap.add(new Paciente("Juan", "Fractura", "C"));
+        heap.add(new Paciente("Maria", "Apendicitis", "A"));
+        
+        Paciente primero = heap.remove();
+        assertEquals("A", primero.getCodigoEmergencia());
+        assertEquals("Maria", primero.toString().split(",")[0].trim());
+    }
 }
